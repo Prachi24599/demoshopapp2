38188bbb3a86355b50be695d92881f7a8579917c
Prachi24599 <prachi24599@gmail.com>
2023-06-10 00:26:03 +0530

    NewProduct Component added

---
+++ src/components/NewProduct.js
@@ -0,0 +1,12 @@
+import ProductForm from "./ProductForm";
+import "./NewProduct.css";
+
+function NewProduct() {
+  return (
+    <div>
+      <ProductForm />
+    </div>
+  );
+}
+
+export default NewProduct;
