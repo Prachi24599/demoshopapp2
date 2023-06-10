5b39b8e85df4b01ae6d6f70a40179756dae9592b
Prachi24599 <prachi24599@gmail.com>
2023-06-10 13:13:08 +0530

    Changing default behaviour of form submit using preventDefault fun

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -2,21 +2,34 @@ import { useState } from "react";
 import "./ProductForm.css";
 
 function ProductForm() {
-  const [title, setTitle] = useState("");
-  const [date, setDate] = useState("");
+  const [newTitle, setTitle] = useState("");
+  const [newDate, setDate] = useState("");
 
   function titleChangeHandler(event) {
-    console.log(event.target.value);
+    // console.log(event.target.value);
     setTitle(event.target.value);
   }
 
   function dateChangeHandler(event) {
     setDate(event.target.value);
-    console.log(event.target.value);
+    // console.log(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    const productData = {
+      title: newTitle,
+      date: newDate,
+    };
+    console.log(productData);
+
+    setTitle("");
+    setDate("");
   }
 
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-product__title">
         <label>Title</label>
         <input type="text" onChange={titleChangeHandler}></input>
